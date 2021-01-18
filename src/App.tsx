488c4babcb13981da4ba3d81c6ef68d2f7d83339
Andrey <andrey@ozorn.in>
2021-01-18 13:20:02 +0100

    layout refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,24 @@ import './Summary/Summary.tsx';
 function App() {
   return (
     <div className="App">
-      <div className="App-layout">
-        <aside className="App-sidebar">
-          <h1 className="App-logo">Q|A</h1>
-          <section className="App-summary">
-            <Summary />
-          </section>
-          <footer className="App-footer">
-            Made by{' '}
-            <a href="https://github.com/oshibka404">Andrey Ozornin</a>{' '}
-            on 17th of January, 2020 as a test assignment for Studocu.
-          </footer>
-        </aside>
-        <div className="App-content">
-          <section className="App-questions">
-            <CreatedQuestions />
-          </section>
-          <section className="App-form">
-            <Form />
-          </section>
-        </div>
+      <aside className="App-sidebar">
+        <h1 className="App-logo">Q|A</h1>
+        <section className="App-summary">
+          <Summary />
+        </section>
+        <footer className="App-footer">
+          Made by{' '}
+          <a href="https://github.com/oshibka404">Andrey Ozornin</a>{' '}
+          on 17th of January, 2020 as a test assignment for Studocu.
+        </footer>
+      </aside>
+      <div className="App-content">
+        <section className="App-questions">
+          <CreatedQuestions />
+        </section>
+        <section className="App-form">
+          <Form />
+        </section>
       </div>
     </div>
   );
